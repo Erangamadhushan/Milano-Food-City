e4314485598a07f3b51a59c24e99d4f038b12e26
Erangamadhushan <erangamadhushann956@gmail.com>
2024-09-11 10:51:24 +0530

    add border-main and border-line properties

--- script/index.js
+++ script/index.js
@@ -19,10 +19,10 @@ let boxes = document.querySelectorAll('.box');
 boxes.forEach(box=> {
   observer.observe(box);
 })
-  window.addEventListener("DOMContentLoaded", function () {
-    renderItem(menu)
-    creatMenuBtn()
-  });
+window.addEventListener("DOMContentLoaded", function () {
+  renderItem(menu)
+  creatMenuBtn()
+});
 
 console.log(menu)
 
@@ -33,7 +33,7 @@ let menuCard = '';
 function renderItem(menuItems){
       menuItems.forEach(item => {
         menuCard += `
-            <div class="col-md-3 col-sm-12 col-lg-3 border border-info menu-item p-1 m-1">
+            <div class="col-md-3 col-sm-12 col-lg-3 border border-line menu-item p-1 m-1">
                 <div class="align-item-center justify-content-center d-flex p-1">
                     <img src='${item.img}' height="150px" class="border border-3 border-info">
                 </div>
